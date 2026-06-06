878c440bb9de7dc55ef7632cfb90daa02b83478f
kirita1223 <s2211988@u.tsukuba.ac.jp>
2026-06-06 14:04:22 +0900

    a

--- envs/mpe_wrapper.py
+++ envs/mpe_wrapper.py
@@ -224,6 +224,14 @@ class PredatorPreyScenario(BaseScenario):
 
     def _agent_reward(self, agent, world):
         rew = 0.0
+        advs = self.adversaries(world)
+        dists = [np.linalg.norm(a.state.p_pos - agent.state.p_pos) for a in advs]
+        min_dist = min(dists)
+        
+        # 1. 距離報酬: 捕食者と離れるとプラス (上限 1.0)
+        # 距離が 1.0 以上離れていれば報酬が頭打ちになるように設定
+        rew += min(min_dist, 1.0)
+        
         for adv in self.adversaries(world):
             if self.is_collision(adv, agent):
                 rew -= 10.0
